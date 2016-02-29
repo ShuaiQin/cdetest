6ec2d8ca3d57b377a93c571862b1b46fe057c6c4
qinshuai <qinshuai@dm-shc-16501160.corp.ebay.com>
2016-02-29 16:30:57 +0800

    update

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class Hello {
-    public static class main{
-        System.log.out("Hello World");
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
