f427babeec72538e0d3631bf0e7606bbe36fcccb
qinshuai <qinshuai@dm-shc-16501160.corp.ebay.com>
2016-03-01 14:54:52 +0800

    test custom rules

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class hello {
     public static void main(String[] args) {
         logger.log("Hello World!");
         logger.log("github push trigger test");
+        String name = "ABCD";
+        name.replace("A","a");
     }
 }
